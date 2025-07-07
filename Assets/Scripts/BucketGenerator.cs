244f645022c0db86992117487755c4a78cb68280
Goon <mrgoonandwatch@gmail.com>
2025-07-06 21:19:45 -0400

    (work in progress) Tried adding image display logic, this is completely broken at the moment.

--- Assets/Scripts/BucketGenerator.cs
+++ Assets/Scripts/BucketGenerator.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BucketGenerator : MonoBehaviour
 {
@@ -21,8 +23,11 @@ public class BucketGenerator : MonoBehaviour
     private const string BucketWall = "Bucket-Wall";
     private const string BucketText = "Bucket-Text";
 
+    private List<string> _imgFilePaths;
+
     void Start()
     {
+        _imgFilePaths = new List<string>();
         _generatedBuckets = new GameObject[0];
         if (StatusDisplay == null)
             StatusDisplay = FindObjectOfType<BucketCountDisplay>().GetComponent<TextMeshProUGUI>();
@@ -68,6 +73,9 @@ public class BucketGenerator : MonoBehaviour
 
         CleanupOldObjects();
 
+        // TODO: Load this list!
+        _imgFilePaths = new List<string> { @"C:\Code\Personal\Art\SanFranciscoRush2049.png", @"C:\Code\Personal\Art\MortalKombatMythologiesSub-Zero.png" };
+
         var length = EndXPosition - StartXPosition;
 
         var wallSpacing = length / NumBuckets;
@@ -93,12 +101,57 @@ public class BucketGenerator : MonoBehaviour
 
     private GameObject GenerateText(float currentXPosition, float textSpacing, int number)
     {
+        var newGameObject = CreateNewBucketPrefab(currentXPosition, textSpacing);
+        var loadedImage = LoadImageTexture(newGameObject, number);
+        SetBucketText(newGameObject, number, loadedImage);
+        
+        return newGameObject;
+    }
+
+    private GameObject CreateNewBucketPrefab(float currentXPosition, float textSpacing)
+    {
         var textPosition = new Vector3(currentXPosition + textSpacing, YPosition, 0);
         var textResource = Resources.Load(BucketText);
         var newGameObject = Instantiate(textResource, textPosition, Quaternion.identity) as GameObject;
+        return newGameObject;
+    }
+
+    private bool LoadImageTexture(GameObject newGameObject, int number)
+    {
+        var imgSprite = newGameObject.GetComponentInChildren<SpriteRenderer>();
+        //Debug.Log($"loading filepath {number} (index {number - 1}) from list of {_imgFilePaths.Count} filepaths");
+        var filepath = number < _imgFilePaths.Count ? _imgFilePaths[number] : "";
+
+        if (imgSprite == null || !System.IO.File.Exists(filepath))
+        {
+            if (imgSprite != null)
+                imgSprite.color = new Color(0, 0, 0, 0);
+            Debug.Log("Skipping loading texture!");
+            return false;
+        }
+
+        imgSprite.color = new Color(1, 1, 1, 1);
+        var textureData = System.IO.File.ReadAllBytes(filepath);
+        var texture = new Texture2D(2, 2);
+        texture.LoadImage(textureData);
+        //imgSprite.texture = texture;
+        //imgSprite.sprite.texture.LoadImage(textureData);
+
+        var pivot = imgSprite.sprite.pivot;
+        var pixelPerUnit = imgSprite.sprite.pixelsPerUnit;
+        uint extrude = 1;
+        
+        imgSprite.sprite = Sprite.Create(texture, imgSprite.sprite.textureRect, pivot, pixelPerUnit, extrude, SpriteMeshType.Tight);
+        imgSprite.sprite.name = $"Bucket{number + 1}-Img-Sprite";
+
+        Debug.Log("Finished loading texture!");
+        return true;
+    }
+
+    private void SetBucketText(GameObject newGameObject, int number, bool imageLoaded)
+    {
         var textComponent = newGameObject.GetComponent<TextMeshPro>();
         textComponent.text = (number + _startingDigit).ToString();
-        return newGameObject;
     }
 
     private void CleanupOldObjects()
