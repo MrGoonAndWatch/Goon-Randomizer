de136d603f40d9aa221cefcfdf8280d326b8cde8
Goon <mrgoonandwatch@gmail.com>
2026-04-01 19:50:05 -0400

    Added support for loading images in to the number slots (not super configurable yet but it works!).

--- Assets/Scripts/BucketGenerator.cs
+++ Assets/Scripts/BucketGenerator.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
+using System.IO;
+using System.Linq;
+using Input = UnityEngine.Input;
 
 public class BucketGenerator : MonoBehaviour
 {
     public TextMeshProUGUI StatusDisplay;
     public TextMeshProUGUI StartingDigitDisplay;
+    [SerializeField]
+    private TMP_InputField _inputField;
 
     public float StartXPosition = -12.0f;
     public float EndXPosition = 12.0f;
@@ -18,6 +21,10 @@ public class BucketGenerator : MonoBehaviour
     private const int MinBuckets = 2;
     private int _startingDigit = -1;
 
+    private string _csvDirectory;
+    // TODO: Eventually maybe add a file picker to select the csv?
+    private const string CsvFilename = "ImagePaths.csv";
+
     private GameObject[] _generatedBuckets;
 
     private const string BucketWall = "Bucket-Wall";
@@ -27,12 +34,15 @@ public class BucketGenerator : MonoBehaviour
 
     void Start()
     {
+        _csvDirectory = Application.persistentDataPath;
         _imgFilePaths = new List<string>();
         _generatedBuckets = new GameObject[0];
         if (StatusDisplay == null)
             StatusDisplay = FindFirstObjectByType<BucketCountDisplay>().GetComponent<TextMeshProUGUI>();
         if (StartingDigitDisplay == null)
             StartingDigitDisplay = FindFirstObjectByType<StartingDigitDisplay>().GetComponent<TextMeshProUGUI>();
+        if (_inputField == null)
+            _inputField = FindFirstObjectByType<FirstDigitInput>().GetComponent<TMP_InputField>();
         FlipStartingNum();
     }
     
@@ -73,8 +83,12 @@ public class BucketGenerator : MonoBehaviour
 
         CleanupOldObjects();
 
-        // TODO: Load this list!
-        _imgFilePaths = new List<string> { @"C:\Code\Personal\Art\SanFranciscoRush2049.png", @"C:\Code\Personal\Art\MortalKombatMythologiesSub-Zero.png" };
+        var firstDigitSpecified = int.TryParse(_inputField?.text, out var firstDigitOffset);
+        var csvFilePath = Path.Combine(_csvDirectory, CsvFilename);
+        if (File.Exists(csvFilePath) && firstDigitSpecified)
+            _imgFilePaths = File.ReadAllLines(csvFilePath).ToList();
+        else
+            _imgFilePaths = new List<string>();
 
         var length = EndXPosition - StartXPosition;
 
@@ -85,7 +99,7 @@ public class BucketGenerator : MonoBehaviour
         var currentXPosition = StartXPosition;
         for (var i = 0; i < NumBuckets - 1; i++)
         {
-            newObjects.Add(GenerateText(currentXPosition, textSpacing, i));
+            newObjects.Add(GenerateText(currentXPosition, textSpacing, i, firstDigitOffset));
 
             currentXPosition += wallSpacing;
 
@@ -94,15 +108,15 @@ public class BucketGenerator : MonoBehaviour
             newObjects.Add(Instantiate(ballResource, wallPosition, Quaternion.identity) as GameObject);
         }
 
-        newObjects.Add(GenerateText(currentXPosition, textSpacing, NumBuckets - 1));
+        newObjects.Add(GenerateText(currentXPosition, textSpacing, NumBuckets - 1, firstDigitOffset));
 
         _generatedBuckets = newObjects.ToArray();
     }
 
-    private GameObject GenerateText(float currentXPosition, float textSpacing, int number)
+    private GameObject GenerateText(float currentXPosition, float textSpacing, int number, int firstDigitOffset)
     {
         var newGameObject = CreateNewBucketPrefab(currentXPosition, textSpacing);
-        var loadedImage = LoadImageTexture(newGameObject, number);
+        var loadedImage = LoadImageTexture(newGameObject, number, firstDigitOffset);
         SetBucketText(newGameObject, number, loadedImage);
         
         return newGameObject;
@@ -116,19 +130,24 @@ public class BucketGenerator : MonoBehaviour
         return newGameObject;
     }
 
-    private bool LoadImageTexture(GameObject newGameObject, int number)
+    private bool LoadImageTexture(GameObject newGameObject, int number, int firstDigitOffset)
     {
+        number += _startingDigit + (firstDigitOffset * 10) - 1;
         var imgSprite = newGameObject.GetComponentInChildren<SpriteRenderer>();
-        //Debug.Log($"loading filepath {number} (index {number - 1}) from list of {_imgFilePaths.Count} filepaths");
-        var filepath = number < _imgFilePaths.Count ? _imgFilePaths[number] : "";
+        Debug.Log($"loading filepath {number} (index {number - 1}) from list of {_imgFilePaths.Count} filepaths");
+        var filepath = number < _imgFilePaths.Count && number >= 0 ? _imgFilePaths[number] : "";
 
         if (imgSprite == null || !System.IO.File.Exists(filepath))
         {
             if (imgSprite != null)
                 imgSprite.color = new Color(0, 0, 0, 0);
+            else
+                Debug.Log("imgSprite is null");
             Debug.Log("Skipping loading texture!");
             return false;
         }
+        
+        Debug.Log("Actually loading texture!");
 
         imgSprite.color = new Color(1, 1, 1, 1);
         var textureData = System.IO.File.ReadAllBytes(filepath);
@@ -141,17 +160,29 @@ public class BucketGenerator : MonoBehaviour
         var pixelPerUnit = imgSprite.sprite.pixelsPerUnit;
         uint extrude = 1;
         
-        imgSprite.sprite = Sprite.Create(texture, imgSprite.sprite.textureRect, pivot, pixelPerUnit, extrude, SpriteMeshType.Tight);
+        var textureRect = new Rect(0, 0, texture.width, texture.height);
+        float xScale = 2.0f;
+        if (textureRect.width > 0)
+            xScale = 2 * imgSprite.sprite.textureRect.width / textureRect.width;
+        float yScale = 2.0f;
+        if (textureRect.height > 0)
+            yScale = 2 * imgSprite.sprite.textureRect.height / textureRect.height;
+        imgSprite.sprite = Sprite.Create(texture, textureRect, pivot, pixelPerUnit, extrude, SpriteMeshType.FullRect);
         imgSprite.sprite.name = $"Bucket{number + 1}-Img-Sprite";
+        imgSprite.gameObject.transform.localScale = new Vector3(xScale, yScale, 1);
+        imgSprite.gameObject.transform.localPosition = new Vector3(255, 255, 0);
 
-        Debug.Log("Finished loading texture!");
+        Debug.Log($"Finished loading texture!");
         return true;
     }
 
     private void SetBucketText(GameObject newGameObject, int number, bool imageLoaded)
     {
         var textComponent = newGameObject.GetComponent<TextMeshPro>();
-        textComponent.text = (number + _startingDigit).ToString();
+        if (imageLoaded)
+            textComponent.text = "";
+        else
+            textComponent.text = (number + _startingDigit).ToString();
     }
 
     private void CleanupOldObjects()
